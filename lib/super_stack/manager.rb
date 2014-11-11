940aab643cf3b6d72458b1901666e7f76bc45080
Laurent B <lbnetid+gh@gmail.com>
2014-11-11 23:34:32 +0100

    Added layers to manager

--- lib/super_stack/manager.rb
+++ lib/super_stack/manager.rb
@@ -1,6 +1,16 @@
 module SuperStack
   class Manager
 
+    attr_reader :layers
+
+    def initialize
+      @layers = {}
+    end
+
+    def to_a
+
+    end
+
 
 
   end
--- spec/manager_spec.rb
+++ spec/manager_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe SuperStack::Manager do
+  subject {SuperStack::Manager.new}
 
-  it 'should contain layers'
+  it 'should contain layers' do
+    expect( subject.respond_to? :layers).to be_truthy
+  end
 
   it 'should present layers ordered by priority'
 
