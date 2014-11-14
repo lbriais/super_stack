d09d8c88d97074fcf2df462d308b0cbfa61a3793
Laurent B <lbnetid+gh@gmail.com>
2014-11-14 13:53:23 +0100

    Override policy verified

--- spec/merge_policies_spec.rb
+++ spec/merge_policies_spec.rb
@@ -4,10 +4,36 @@ describe SuperStack::MergePolicies do
 
   subject {SuperStack::MergePolicies}
 
+  def file_from_layer(layer_number)
+    File.expand_path("../../test/stacked_layer_#{layer_number}.yml", __FILE__)
+  end
+
+  (1..4).each do |layer_number|
+    let("layer#{layer_number}".to_sym) {
+      file_name = file_from_layer layer_number
+      Hash[YAML::load(File.open(file_name)).map { |k, v| [k.to_sym, v] }]
+    }
+  end
+
+
   it 'should have merge policies' do
     policies = subject.list
     expect( policies.is_a? Array).to be_truthy
     expect( policies.count == 2).to be_truthy
+  end
+
+  context 'when dealing with override policy' do
+
+    subject {SuperStack::MergePolicies::OverridePolicy}
+
+
+
+    it 'should retain only the second hash' do
+      merged_hashs = subject.merge(layer1, layer2)
+      expect( merged_hashs == layer2).to be_truthy
+      expect( merged_hashs[:layer] == 'two').to be_truthy
+    end
+
   end
 
 
